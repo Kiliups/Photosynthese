d96fd4c57df22069819c6bbdeed1ba4424c47ffb
Eva Wimmer <eva.wimmer@st.oth-regensburg.de>
2023-05-10 09:23:36 +0200

    add google-services.json

--- app/src/main/java/com/othregensburg/photosynthese/MainActivity.kt
+++ app/src/main/java/com/othregensburg/photosynthese/MainActivity.kt
@@ -2,6 +2,7 @@ package com.othregensburg.photosynthese
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
