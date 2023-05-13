9acfe84c1dea95e0601e6d5de417baf43ec85472
Kiliups <Mayer-kilian@gmx.de>
2023-05-13 20:28:58 +0200

    insert function done

--- app/src/main/java/com/othregensburg/photosynthese/models/mediaViewModel.kt
+++ app/src/main/java/com/othregensburg/photosynthese/models/mediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.othregensburg.photosynthese.models
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -12,10 +13,34 @@ import kotlinx.coroutines.launch
 
 class mediaViewModel(application: Application) : AndroidViewModel(application) {
     var storageRef: StorageReference = FirebaseStorage.getInstance().getReference()
-    val database = FirebaseFirestore.getInstance()
+    val db = FirebaseFirestore.getInstance()
 
     //inserts Media into Firestore and Firebase Storage
     fun insert(media: Media)=viewModelScope.launch(Dispatchers.IO){
+        //generete new document in Firestore and store id
+        val mediaId = db.collection("media").document().id
+        media.id = mediaId
+
+        //set reference to path in Firebase
+        val reference = "media/${media.event_id}/${mediaId}.jpg"
+        media.reference = reference
+
+        //format media object so Uri doesn't get into Firestore
+        val uploadMedia= mapOf(
+            "id" to media.id,
+            "event_id" to media.event_id,
+            "reference" to media.reference,
+            "timestamp" to media.timestamp,
+            "user" to media.user
+        )
+
+        //insert formated media into Firestore
+        db.collection("media").document(mediaId)
+            .set(uploadMedia)
+
+        //insert Uri in Firebase Storage
+        storageRef.child("${reference}").putFile(media.content!!)
+
     }
 
     //deletes given Media from Firebase
