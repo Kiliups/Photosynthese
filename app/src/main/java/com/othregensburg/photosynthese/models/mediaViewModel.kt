d5581d371898a9a207211215215648ad5f985cb8
Kiliups <Mayer-kilian@gmx.de>
2023-05-15 14:16:51 +0200

    add mediaViewModel delete and insert function

--- app/src/main/java/com/othregensburg/photosynthese/models/mediaViewModel.kt
+++ app/src/main/java/com/othregensburg/photosynthese/models/mediaViewModel.kt
@@ -10,48 +10,90 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class mediaViewModel(application: Application) : AndroidViewModel(application) {
     var storageRef: StorageReference = FirebaseStorage.getInstance().getReference()
     val db = FirebaseFirestore.getInstance()
 
-    //inserts Media into Firestore and Firebase Storage
-    fun insert(media: Media)=viewModelScope.launch(Dispatchers.IO){
+    //inserts media into firestore and firebase Storage
+    fun insert(media: Media) = viewModelScope.launch(Dispatchers.IO) {
 
-        //generete new document in Firestore and store id
+        //generete new document in firestore and store id
         val mediaId = db.collection("media").document().id
         media.id = mediaId
 
-        //set reference to path in Firebase
+        //set reference to path in firebase
         val reference = "media/${media.event_id}/${mediaId}.jpg"
         media.reference = reference
 
-        //format media object so Uri doesn't get into Firestore
-        val uploadMedia= mapOf(
-            "id" to media.id,
+        //format media object so uri doesn't get into firestore
+        val uploadMedia = mapOf(
             "event_id" to media.event_id,
             "reference" to media.reference,
             "timestamp" to media.timestamp,
             "user" to media.user
         )
 
-        //insert formated media into Firestore
+        //insert formated media into firestore
         db.collection("media").document(mediaId)
             .set(uploadMedia)
 
-        //insert Uri in Firebase Storage
+        //insert Uri in firebase storage
         storageRef.child("${reference}").putFile(media.content!!)
 
     }
 
     //deletes given Media from Firebase
-    fun delete(media: Media)=viewModelScope.launch(Dispatchers.IO){
+    fun delete(media: Media) = viewModelScope.launch(Dispatchers.IO) {
 
+        //delete data from firestore
+        db.collection("media").document(media.id!!).delete()
+
+        //delete data from firebase storage
+        storageRef.child(media.reference!!).delete()
     }
 
-    //gets all Media for given Event from Firebase
-    fun getEventMedia(event_id:String?="0"): MutableLiveData<List<Media>> {
-        var result:MutableLiveData<List<Media>>  =  MutableLiveData()
+    //gets all media for given event from firebase
+    fun getEventMedia(event_id: String? = "0"): MutableLiveData<List<Media>> {
+
+        var result: MutableLiveData<List<Media>> = MutableLiveData()
+
+        //get media from firebase for given event_id in chronological order
+        db.collection("media").whereEqualTo("event_id", event_id).orderBy("timestamp")
+            .get().addOnSuccessListener { documents ->
+
+                //help list to store media objects into this List
+                val mediaList = mutableListOf<Media>()
+
+                //launch new Coroutine that all result are in right order
+                viewModelScope.launch(Dispatchers.IO) {
+
+                    //create new media object for each document returned by Firebase
+                    for (item in documents) {
+
+                        val media = Media(
+                            item.id as String?,
+                            item.get("event_id") as String?,
+                            item.get("reference") as String?,
+                            item.get("timestamp") as Long?,
+                            item.get("user") as String?,
+                            null
+                        )
+
+                        //get picture uri for each media object from firebase storage and wait until it is downloaded
+                        val uri = storageRef.child(media.reference!!).downloadUrl.await()
+                        media.content = uri
+
+                        //after media uri is downloaded add media object to help list
+                        mediaList.add(media)
+                    }
+
+                    //after ever media object is downloaded set live data equal to help list
+                    result.value = mediaList
+                }
+            }
+
         return result
     }
 }
